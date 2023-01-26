09825b82c43f73ffac5e1e451687b6b26c001210
william berry <wirrzbow@gmail.com>
2023-01-25 21:33:28 -0500

    added new codewars challenge

---
+++ only-evens.js
@@ -0,0 +1,5 @@
+//Create an array of numbers
+let nums = [1,2,3,4,5,6,7,8,9,10]
+//Return a new array of numbers that includes every even number from the previous Arrays
+let onlyEvens = nums.filter(n=>n%2===0)
+alert([onlyEvens])
