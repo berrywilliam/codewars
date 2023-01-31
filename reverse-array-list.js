863b29764145818e49521bd991f684ca23cc814d
william berry <wirrzbow@gmail.com>
2023-01-30 19:52:51 -0500

    reversing the order of an array

---
+++ reverse-array-list.js
@@ -0,0 +1,9 @@
+//Arrays
+//Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
+
+// My Solution : 
+
+function poke(partyList){
+    return partyList.reverse()
+}
+poke(['hey', 'why', 'so', 'serious')  //["serious", "so", "why", "hey"]
