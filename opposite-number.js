004ce6451c88ff8608f092f28b21342e0bb8d893
william berry <wirrzbow@gmail.com>
2023-01-31 20:30:36 -0500

    added a new codewars challenge to find the opposite of a number

---
+++ opposite-number.js
@@ -0,0 +1,11 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// function opposite(number) {
+//   }
+
+// My Solution: 
+
+function opposite(number) {
+    let opp = -number
+    return opp
+  }
